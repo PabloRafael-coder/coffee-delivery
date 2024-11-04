d5033a3418609781df49f5f8829c6c1f7780de21
PabloRafael-coder <pabloofael@gmail.com>
2024-11-04 12:34:34 -0300

    feat: Adicionar todos os estilos de guia no projeto

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -20,11 +20,21 @@ export const GlobalStyle = createGlobalStyle`
     --base-card: #f3f2f2;
     --background: #fafafa;
     --white: #ffffff;
-    --title-xl: 3rem;
-    --title-l: 2rem;
-    --title-m: 1.5rem;
-    --title-s: 1.25rem;
-    --title-xs: 1.125rem;
+
+    --fs-title-xl: 3rem;
+    --fs-title-l: 2rem;
+    --fs-title-m: 1.5rem;
+    --fs-title-s: 1.25rem;
+    --fs-title-xs: 1.125rem;
+
+    --fs-text-l: 1.25rem;
+    --fs-text-m: 1rem;
+    --fs-text-s: 0.875rem;
+    --fs-text-xs: 0.75rem;
+    
+    --ff-sans-roboto: 'Roboto', sans-serif;  
+    --fw-extrabold: 'bolder';
+    --fw-bold: 'bold';
   }
 
   * {
