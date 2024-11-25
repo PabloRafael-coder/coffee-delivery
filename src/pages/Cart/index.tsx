7e94fe94b5c4a1d4457a5ef36a9800b091bce28d
PabloRafael-coder <pabloofael@gmail.com>
2024-11-24 22:51:17 -0300

    feat: Adicionar os elementos ao componente Cart

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,3 +1,42 @@
+import { CurrencyDollar, MapPinLine } from '@phosphor-icons/react';
+import {
+  AddressDetails,
+  CartContainer,
+  FormContainer,
+  PayDetails,
+  PayMethodContainer,
+} from './styles';
+import { InputText } from './InputText';
+
 export function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <CartContainer>
+      <section>
+        <h2>Complete seu pedido</h2>
+        <FormContainer>
+          <div>
+            <MapPinLine />
+            <AddressDetails>
+              <p>Endereço de Entrega</p>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </AddressDetails>
+          </div>
+          <form action="">
+            <InputText />
+          </form>
+        </FormContainer>
+        <PayMethodContainer>
+          <div>
+            <CurrencyDollar />
+            <PayDetails>
+              <p>Pagamento</p>
+              <p>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </p>
+            </PayDetails>
+          </div>
+        </PayMethodContainer>
+      </section>
+    </CartContainer>
+  );
 }
---
+++ src/pages/Cart/styles.ts
@@ -0,0 +1,80 @@
+import styled from 'styled-components';
+
+export const CartContainer = styled.main`
+  width: min(90rem, 100% - 2 * 10rem);
+  margin-inline: auto;
+  border: solid 1px black;
+
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: 0.75rem;
+    max-width: 40rem;
+  }
+
+  h2 {
+    font-family: var(--ff-sans-baloo);
+    font-size: var(--fs-title-xs);
+  }
+`;
+
+const StyleBaseSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  background: var(--base-card);
+  padding: 2.5rem;
+  border-radius: 0.375rem;
+`;
+
+export const FormContainer = styled(StyleBaseSection)`
+  margin-top: 1rem;
+
+  > div {
+    display: flex;
+    gap: 0.5rem;
+
+    & svg {
+      color: var(--yellow-dark);
+      width: 22px;
+      height: 22px;
+    }
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const StylesBaseDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.125rem;
+
+  :nth-child(1) {
+    font-size: var(--fs-text-m);
+    color: var(--base-subtitle);
+  }
+
+  :nth-child(2) {
+    font-size: var(--fs-text-s);
+    color: var(--base-text);
+  }
+`;
+
+export const AddressDetails = styled(StylesBaseDetails)``;
+
+export const PayMethodContainer = styled(StyleBaseSection)`
+  > div {
+    display: flex;
+    gap: 0.5rem;
+  }
+  & svg {
+    color: var(--purple);
+    width: 22px;
+    height: 22px;
+  }
+`;
+
+export const PayDetails = styled(StylesBaseDetails)``;
