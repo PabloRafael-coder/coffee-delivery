a5a487c37a26da58edc1c862badc919328214c8d
PabloRafael-coder <pabloofael@gmail.com>
2024-12-31 18:18:22 -0300

    refactor: Refatorar o estado do contexto mudando de useState para useReducer.

--- src/contexts/CoffeeContext.tsx
+++ src/contexts/CoffeeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, type ReactNode } from 'react';
+import { createContext, useReducer, type ReactNode } from 'react';
 
 export interface Coffee {
   id: number;
@@ -33,54 +33,75 @@ interface CoffeeContextProviderProps {
 export function CoffeeContextProvider({
   children,
 }: CoffeeContextProviderProps) {
-  const [cart, setCart] = useState<Coffee[]>([]);
+  const [cart, dispatch] = useReducer((state: Coffee[], action: any) => {
+    console.log(state);
+    console.log(action);
+
+    if (action.type === 'ADD_NEW_COFFEE_CART') {
+      return [...state, { ...action.payload }];
+    }
+
+    return state;
+  }, []);
+
   const totalQuantityCoffee = cart.length;
 
   function createNewCoffeeOrder({ coffee, quantity }: CatalogProps) {
-    setCart((state) => {
-      const isCoffeeInCart = state.find((item) => item.id === coffee.id);
+    // setCart((state) => {
+    //   const isCoffeeInCart = state.find((item) => item.id === coffee.id);
+    //   if (isCoffeeInCart) {
+    //     return state.map((item) =>
+    //       item.id === coffee.id
+    //         ? { ...item, quantity: item.quantity + quantity }
+    //         : item,
+    //     );
+    //   } else {
+    //     return [...state, { ...coffee, quantity }];
+    //   }
+    // });
 
-      if (isCoffeeInCart) {
-        return state.map((item) =>
-          item.id === coffee.id
-            ? { ...item, quantity: item.quantity + quantity }
-            : item,
-        );
-      } else {
-        return [...state, { ...coffee, quantity }];
-      }
+    dispatch({
+      type: 'ADD_NEW_COFFEE_CART',
+      payload: {
+        ...coffee,
+        quantity,
+      },
     });
   }
 
   function increaseQuantityItemInCart(itemId: number) {
-    setCart((state) =>
-      state.map((coffee) => {
-        if (coffee.quantity < 20 && coffee.id === itemId) {
-          return { ...coffee, quantity: coffee.quantity + 1 };
-        }
-
-        return { ...coffee };
-      }),
-    );
+    // setCart((state) =>
+    //   state.map((coffee) => {
+    //     if (coffee.quantity < 20 && coffee.id === itemId) {
+    //       return { ...coffee, quantity: coffee.quantity + 1 };
+    //     }
+    //     return { ...coffee };
+    //   }),
+    // );
+    dispatch({
+      type: 'INCREASE_QUANTITY_ITEM_IN_CART',
+      payload: {
+        itemId,
+      },
+    });
   }
 
   function decreaseQuantityItemInCart(itemId: number) {
-    setCart((state) =>
-      state.map((coffee) => {
-        if (coffee.quantity > 1 && coffee.id === itemId) {
-          return { ...coffee, quantity: coffee.quantity - 1 };
-        }
-
-        return { ...coffee };
-      }),
-    );
+    // setCart((state) =>
+    //   state.map((coffee) => {
+    //     if (coffee.quantity > 1 && coffee.id === itemId) {
+    //       return { ...coffee, quantity: coffee.quantity - 1 };
+    //     }
+    //     return { ...coffee };
+    //   }),
+    // );
   }
 
   function removeCoffeeOrder(itemId: number) {
-    const removeCoffeeCart = cart.filter((coffee) => {
-      return coffee.id !== itemId;
-    });
-    setCart(removeCoffeeCart);
+    // const removeCoffeeCart = cart.filter((coffee) => {
+    //   return coffee.id !== itemId;
+    // });
+    // setCart(removeCoffeeCart);
   }
 
   return (
